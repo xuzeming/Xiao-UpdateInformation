2b14f2609cc6f3ae33abb8e85bfd0e2206c036e7
Administrator <Administrator@PC-20181023FSAX>
2019-01-24 21:10:13 +0800

    王徐 银行卡的绑定/解绑,

--- src/main/java/com/zl/SecuritiesSystem/SecuritiesSystemApplication.java
+++ src/main/java/com/zl/SecuritiesSystem/SecuritiesSystemApplication.java
@@ -9,12 +9,13 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@MapperScan("com.zl.mapper")
+@MapperScan("com.zl.SecuritiesSystem.dao")
 @ComponentScan(basePackages="com.zl")
 public class SecuritiesSystemApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(SecuritiesSystemApplication.class, args);
 	}
+	
 	/**
 	 * 重写启动类
 	 */
